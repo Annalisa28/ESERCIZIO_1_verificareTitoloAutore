2ee40eb277ae4038b22a9dc9a7f9b9f5627d2171
Annalisa28 <120007375+Annalisa28@users.noreply.github.com>
2023-03-06 15:38:44 +0100

    esercizio 1 completato

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+/*
+Esercizio
+Provate a scrivere il codice per verificare titolo e autore di un certo
+volume contengonoo una certa sequenza di caratteri:
+concatenate titolo e autore
+convertite le maiuscole in minuscole
+verificate la presenza della stringa
+I tre elementi (autore, titolo e stringa) possono essere contenuti in tre
+variabili. Cercate di scrivere un’unica riga per i tre passi precedenti,
+componendo le funzioni con il punto (.). Non utilizzate form per
+l’inserimento dei parametri, ma impostate i valori nelle variabili.
+*/
+
 // Import stylesheets
 import './style.css';
 
 // Write Javascript code!
-let autore= "Mario";
-let titolo= "Nuvole";
-let stringa= "ma"
+let autore= "Mario";   //nome autore
+let titolo= "Nuvole";  //nome titolo
+let stringa= "ma";      //stringa inserita dall'utente
 
-let concatenazione= 
+if(autore + " " + titolo.toLowerCase().includes(stringa.toLowerCase()))
+{
+  console.log("Autore:" + autore + "\" " + "Titolo:" + titolo + "\"");
+}
+else{
+  console.log("Nessun risultato trovato");
+}
 
